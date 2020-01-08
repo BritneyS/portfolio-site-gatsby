8fbe2e8a756ee0d9e2df103f8251ad6e431e78aa
Britney Smith <brit.smith7@gmail.com>
2020-01-08 11:44:00 -0500

    Changed header's text color

--- src/assets/jss/material-kit-react/components/headerStyle.jsx
+++ src/assets/jss/material-kit-react/components/headerStyle.jsx
@@ -69,7 +69,7 @@ const headerStyle = {
   },
   primary: {
     backgroundColor: primaryColor,
-    color: "#FFFFFF",
+    color: "#851de0",
     boxShadow:
       "0 4px 20px 0px rgba(0, 0, 0, 0.14), 0 7px 12px -5px rgba(156, 39, 176, 0.46)"
   },
