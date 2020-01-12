317e3db533a5064b51d0e2dc532d2f5d75f02033
Britney Smith <brit.smith7@gmail.com>
2020-01-12 13:56:36 -0500

    Abstracted header copy

---
+++ src/utils/copy.js
@@ -0,0 +1,27 @@
+const copy = {
+    Header: {
+        brandTitle: "Britney Smith"
+    },
+    HeaderTitleItems: {
+        projectsLinkTitle: "Projects",
+        projectsLink: "#projects",
+        resumeLinkTitle: "Resume",
+        resumeLink: "#resume",
+        contactLinkTitle: "Contact Me",
+        contactLink: "#contact",
+    },
+    HeaderIconItems: {
+        twitterTooltip: "Find me on Twitter",
+        twitterIconLink: "https://twitter.com/tippitytap1",
+        githubTooltip: "Find me on Github",
+        githubIconLink: "https://github.com/BritneyS",
+        devtoToolTip: "Find me on Dev.to",
+        devtoIconLink: "https://dev.to/britneys",
+        linkedinToolTip: "Find me on LinkedIn",
+        linkedinIconLink: "https://www.linkedin.com/in/smithbritney/",
+        emailTooltip: "Email me",
+        emailIconLink: "mailto: brit.smith7@gmail.com"
+    }
+};
+
+export default copy;
